091970d257909747a5e08b5a6932e092d9fdba79
MotoyaKimura <c5624188@aoyama.jp>
2024-10-21 18:52:05 +0900

    途中コミット

---
+++ assimp/build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Motoya_Kimura/Downloads/assimp/contrib/zlib
+# Build directory: C:/Users/Motoya_Kimura/Downloads/assimp/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
